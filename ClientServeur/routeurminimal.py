26e0b14c4d65c364f3b6a0a8a5dca5583c28544c
Chris Arnault <chris.arnault.1@gmail.com>
2023-08-02 16:44:38 +0200

    minimal server

--- ClientServeur/routeurminimal.py
+++ ClientServeur/routeurminimal.py
@@ -6,6 +6,19 @@ import asyncio
 
 i = 0
 
+async def hello(request):
+    global i
+    text = "Hello, world i={}".format(i)
+    i += 1
+    return web.Response(text=text)
+
+app = web.Application()
+app.add_routes([web.get('/', hello)])
+
+web.run_app(app, port=5000, host='192.168.142.135')
+
+'''
+
 async def stream(request):
     async def generate():
         while True:
@@ -18,14 +31,15 @@ async def stream(request):
     return web.Response(body=generate(), content_type='multipart/x-mixed-replace; boundary=frame')
 
 app = web.Application()
-app.router.add_get('/video_feed', stream)
+app.router.add_get('/', stream)
 
 if __name__ == '__main__':
     try:
-        web.run_app(app, host='192.168.4.3', port='5000')
+        web.run_app(app, host='192.168.4.2', port='5000')
     except:
         print("cannot start the routeur")
     
 # sudo iptables -A INPUT -p tcp --dport 5000 -j ACCEPT
 
 
+'''
